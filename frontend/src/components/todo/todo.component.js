a70de80e8de7565151008062da1c0a8d2ec57ba9
Setthawut Pimthong <oop_best@yahoo.com>
2026-03-12 11:51:49 +0700

    feat: UX Improvements: Loading State & Error Handling

--- frontend/src/components/todo/todo.component.js
+++ frontend/src/components/todo/todo.component.js
@@ -11,13 +11,22 @@ angular.module('todoApp').component('todoApp', {
       $ctrl.todos = [];
       $ctrl.newTodo = '';
       $ctrl.statusFilter = '';
+      $ctrl.isLoading = true;
+      $ctrl.isSaving = false;
+      $ctrl.error = null;
       loadTodos();
     };
 
     // โหลดข้อมูลจาก Backend
     function loadTodos() {
+      $ctrl.isLoading = true;
+      $ctrl.error = null;
       TodoService.getTodos().then(function(data) {
         $ctrl.todos = data;
+      }).catch(function(err) {
+        $ctrl.error = 'ไม่สามารถโหลดข้อมูลจากเซิร์ฟเวอร์ได้';
+      }).finally(function() {
+        $ctrl.isLoading = false;
       });
     }
 
@@ -38,33 +47,48 @@ angular.module('todoApp').component('todoApp', {
     // เพิ่ม Todo ใหม่
     $ctrl.addTodo = function() {
       const text = $ctrl.newTodo.trim();
-      if (!text) return;
+      if (!text || $ctrl.isSaving) return;
+
+      $ctrl.isSaving = true;
+      $ctrl.error = null;
 
       TodoService.addTodo(text).then(function(savedTodo) {
         $ctrl.todos.unshift(savedTodo); 
         $ctrl.newTodo = ''; // เคลียร์ input
+      }).catch(function() {
+        $ctrl.error = 'ไม่สามารถเพิ่มรายการได้ โปรดลองอีกครั้ง';
+      }).finally(function() {
+        $ctrl.isSaving = false;
       });
     };
 
     // อัปเดตเมื่อติ๊ก Checkbox
     $ctrl.updateTodo = function(todo) {
+      $ctrl.error = null;
       TodoService.updateTodo(todo._id, todo.done, todo.text).catch(function() {
         todo.done = !todo.done;
+        $ctrl.error = 'ไม่สามารถอัปเดตสถานะได้';
       });
     };
 
     // ลบ 1 รายการ
     $ctrl.removeTodo = function(id) {
+      $ctrl.error = null;
       TodoService.deleteTodo(id).then(function() {
         $ctrl.todos = $ctrl.todos.filter(function(todo) {
           return todo._id !== id;
         });
+      }).catch(function() {
+        $ctrl.error = 'ไม่สามารถลบรายการได้';
       });
     };
 
     $ctrl.clearDone = function() {
+      $ctrl.error = null;
       TodoService.clearDone().then(function() {
         loadTodos();
+      }).catch(function() {
+        $ctrl.error = 'ไม่สามารถลบรายการที่ทำเสร็จแล้วได้';
       });
     };
 
